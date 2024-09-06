7192f39672af8f32c3acf85bf16f27417bdb170a
devsanthoshmk <kavimanisan@gmail.com>
2024-09-06 16:52:29 +0530

    generative func

--- prerequisites/async.py
+++ prerequisites/async.py
@@ -12,9 +12,8 @@ async def main():
 def a(s):
     print(s)
 a(asyncio.run(count()))
-
-st=time.time()
-asyncio.run(main())
+# st=time.time()
+# asyncio.run(main())
 # print("\ntime taken",time.time()-st)
 # st=time.time()
 
--- prerequisites/generative functions.py
+++ prerequisites/generative functions.py
@@ -1,10 +1,8 @@
 def listing():
-    yield 1
-    yield 2
-    yield 3
-    yield 3
-    yield 3
-    yield 3
+    a=0
+    while True:
+        yield a
+        a+=1    
 obj=listing()
 print(obj.__next__())
 print(obj.__next__())
